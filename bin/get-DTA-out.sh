1445c32fd43617d9654f5ab0ca154c35d6d9a469
Mike Mayers <mmayers@scripps.edu>
2016-06-13 17:49:07 -0700

    Using garibadli HostName to connect, rather than explicit user@server

--- bin/get-DTA-out.sh
+++ bin/get-DTA-out.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SOURCEDIR=mmayers@garibaldi:"/gpfs/home/mmayers/proteomics/N15_mouse_studies/"
+SOURCEDIR=garibaldi:"/gpfs/home/mmayers/proteomics/N15_mouse_studies/"
 
 rsync -av --exclude="*UC13" --include="*/" --include="DTASelect-filter*.txt" --exclude="*" --prune-empty-dirs $SOURCEDIR .
 
