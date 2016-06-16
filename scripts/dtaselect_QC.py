0b36079a388624128e4af61841f4892421adff5e
Mike Mayers <mmayers@scripps.edu>
2016-06-16 15:26:11 -0700

    Made plot functions modular for import into Jupyter Notebook

--- scripts/dtaselect_QC.py
+++ scripts/dtaselect_QC.py
@@ -6,10 +6,11 @@ Generate qc plots using dtaselect-filter.txt
 """
 import os
 import matplotlib
-matplotlib.use('Agg')
+if __name__ == '__main__':
+    matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from itertools import chain
-from . import blazmass_tools
+from metaproteomics.file_processing import blazmass_tools
 import numpy as np
 from collections import Counter
 import sys
@@ -61,16 +62,35 @@ def plot_bar(x, y, title, xlabel, ylabel, save=None):
         plt.savefig(os.path.join(save, title.replace("\n","_") + ".png"), orientation='landscape', dpi=300)
         plt.savefig(os.path.join(save, title.replace("\n","_") + ".pdf"), orientation='landscape')
 
+def get_name_parser(dta_path):
+    dta_path = os.path.abspath(os.path.expanduser(dta_path))
+    name = os.path.basename(os.path.dirname(dta_path))
+    dta_parser = list(blazmass_tools.dta_select_parser(dta_path))
+    return name, dta_parser
+
+def plot_ppm(dta_path, name = '', dta_parser = None, save = None):
+    # method for plotting ppm, can be imported and used in notebooks
+    if not name or not dta_parser:
+        name, dta_parser = get_name_parser(dta_path)
+
+    ppm_error = list(chain(*[[x['PPM'] for x in locus['peptides']] for locus in dta_parser]))
+    plot_hist(ppm_error, 'ppm error\n{}'.format(name), 'ppm', 'count', save=save)
+
+def plot_psm(dta_path, name = '', dta_parser = None, save = None):
+    # method for plotting psms, can be imported and used in notebooks
+    if not name or not dta_parser:    
+        name, dta_parser = get_name_parser(dta_path)
+        
+    step,num_PSM = num_PSM_per_scan(dta_parser)
+    plot_bar(step, num_PSM, "Number of PSMs per LC-Step\n{}".format(name), "LC-Step", "Number of PSMs", save=save)
+
 if len(sys.argv) == 2:
-    dta_path = os.path.expanduser(sys.argv[1])
+    dta_path = sys.argv[1]
 else:
     dta_path = os.getcwd()
 
 if __name__ == "__main__":
-    name = os.path.basename(dta_path)
-    dta_parser = list(blazmass_tools.dta_select_parser(os.path.join(dta_path, "DTASelect-filter.txt")))
-    ppm_error = list(chain(*[[x['PPM'] for x in locus['peptides']] for locus in dta_parser]))
-    plot_hist(ppm_error, 'ppm error\n{}'.format(name), 'ppm', 'count', save=dta_path)
-    
-    step,num_PSM = num_PSM_per_scan(dta_parser)
-    plot_bar(step, num_PSM, "Number of PSMs per LC-Step\n{}".format(name), "LC-Step", "Number of PSMs", dta_path)
+    name, dta_parser = get_name_parser(os.path.join(dta_path, 'DTASelect-filter.txt'))  
+    plot_ppm(dta_path, name, dta_parser, save = dta_path)    
+    plot_psm(dta_path, name, dta_parser, save = dta_path)
+
